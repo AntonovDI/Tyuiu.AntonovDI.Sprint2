f41849e3fafd4dfa4ab2407fd20707d8103a80f2
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-12 17:01:17 +0500

    Task2

---
+++ Tyuiu.AntonovDI.Sprint2.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AntonovDI.Sprint2.Task1.V21.Lib
+{
+    public class DataService : ISprint2Task1V21
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (b == c);
+            res[1] = (a != b) & (d >= a);
+            res[2] = (a == b) || (d >= a);
+            res[3] = (a <= b) && (d >= c);
+            res[4] = !(res[0]);
+            res[5] = (a == c) ^ (c > d);
+            return res;
+        }
+    }
+}
