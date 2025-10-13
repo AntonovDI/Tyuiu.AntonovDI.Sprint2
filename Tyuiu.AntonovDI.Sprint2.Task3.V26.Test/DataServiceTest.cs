fa8916139336a352a46274e0f4ff3372777f0bd2
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-13 20:56:23 +0500

    Task3

---
+++ Tyuiu.AntonovDI.Sprint2.Task3.V26.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AntonovDI.Sprint2.Task3.V26.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint2.Task3.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 0.2;
+            double res = ds.Calculate(x);
+            double wait = 1.237;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1.667;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+        double x = -1.0;
+        double res = ds.Calculate(x);
+        double wait = -4.754;
+        Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -31;
+            double res = ds.Calculate(x);
+            double wait = -247.968;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint2.Task3.V26/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AntonovDI.Sprint2.Task3.V26.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *"); 
+        Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+        Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+        Console.WriteLine("* трех знаков после запятой;                                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение переменной X:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.Calculate(x);
+        Console.WriteLine("Значение выражения - " + Math.Round(res, 3));
+    }
+}
