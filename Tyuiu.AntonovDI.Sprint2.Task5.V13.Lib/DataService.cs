6aad6680bd26a8614b8064b338f65fd8f6d8cfe0
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-15 19:29:45 +0500

    Task5

---
+++ Tyuiu.AntonovDI.Sprint2.Task5.V13.Lib/DataService.cs
@@ -0,0 +1,90 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.AntonovDI.Sprint2.Task5.V13.Lib;
+
+public class DataService : ISprint2Task5V13
+{
+    public string FindDateOfNextDay(int g, int m, int n)
+    {
+        if (!(m >= 1 && m <= 12))
+            throw new ArgumentException("Íĺęîđđĺęňíűé íîěĺđ ěĺń˙öŕ");
+
+        int[] monthesDays = new int[] { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (n > monthesDays[m])
+            throw new ArgumentException("Íĺęîđđĺęňíîĺ ÷čńëî");
+
+        int next_m = m;
+        if (n + 1 > monthesDays[m])
+        {
+            switch (m)
+            {
+                case 1:
+                    {
+                        next_m = 2;
+                        break;
+                    }
+                case 2:
+                    {
+                        next_m = 3;
+                        break;
+                    }
+                case 3:
+                    {
+                        next_m = 4;
+                        break;
+                    }
+                case 4:
+                    {
+                        next_m = 5;
+                        break;
+                    }
+                case 5:
+                    {
+                        next_m = 6;
+                        break;
+                    }
+                case 6:
+                    {
+                        next_m = 7;
+                        break;
+                    }
+                case 7:
+                    {
+                        next_m = 8;
+                        break;
+                    }
+                case 8:
+                    {
+                        next_m = 9;
+                        break;
+                    }
+                case 9:
+                    {
+                        next_m = 10;
+                        break;
+                    }
+                case 10:
+                    {
+                        next_m = 11;
+                        break;
+                    }
+                case 11:
+                    {
+                        next_m = 12;
+                        break;
+                    }
+                case 12:
+                    {
+                        next_m = 1;
+                        break;
+                    }
+            }
+            n = 1;
+        }
+        else
+        {
+            n++;
+        }
+        return $"{n:D2}.{next_m:D2}.{g}";
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint2.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AntonovDI.Sprint2.Task5.V13.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint2.Task5.V13.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void ValidDateOfNextDay()
+    {
+        var ds = new DataService();
+
+        Assert.AreEqual("23.09.2024", ds.FindDateOfNextDay(2024, 9, 22));
+        Assert.AreEqual("01.10.2024", ds.FindDateOfNextDay(2024, 9, 30));
+        Assert.AreEqual("01.11.2024", ds.FindDateOfNextDay(2024, 10, 31));
+        Assert.AreEqual("01.03.2024", ds.FindDateOfNextDay(2024, 2, 29));
+
+        Assert.ThrowsException<ArgumentException>(() => ds.FindDateOfNextDay(2024, 2, 30));
+        Assert.ThrowsException<ArgumentException>(() => ds.FindDateOfNextDay(2024, 13, 30));
+    }
+}
